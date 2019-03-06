0eab6679dfaf11e7a9fec5da1c7877fd17db66bc
crypto-jones <integral.being@gmail.com>
2019-03-06 15:01:19 -0800

    Add NavBar icons

--- src/components/globals/navbar/NavbarIcons.js
+++ src/components/globals/navbar/NavbarIcons.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import { FaInstagram, FaTwitter, FaFacebook } from 'react-icons/fa';
+import '../../../sass/main.scss';
 
 class NavbarIcons extends Component {
-  state = {};
+  state = {
+    icons: [
+      {
+        id: 1,
+        icon: <FaFacebook className="icon facebook-icon " />,
+        path: 'https://www.facebook.com',
+      },
+      {
+        id: 2,
+        icon: <FaTwitter className="icon twitter-icon " />,
+        path: 'https://www.twitter.com',
+      },
+      {
+        id: 3,
+        icon: <FaInstagram className="icon instagram-icon " />,
+        path: 'https://www.instagram.com',
+      },
+    ],
+  };
   render() {
-    return <div />;
+    return (
+      <div className="icon-wrapper">
+        {this.state.icons.map(item => {
+          return (
+            <a
+              href={item.path}
+              key={item.id}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          );
+        })}
+      </div>
+    );
   }
 }
 
