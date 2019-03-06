9f591a80102af37f1d30d667a9958917a616a5ff
crypto-jones <integral.being@gmail.com>
2019-03-05 19:18:55 -0800

    Build dynamic links

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
+import { Link } from 'gatsby';
+import '../../../sass/main.scss';
 
 class NavbarLinks extends Component {
-  state = {};
+  state = {
+    links: [
+      {
+        id: 0,
+        path: '/',
+        name: 'home',
+      },
+      {
+        id: 1,
+        path: '/about',
+        name: 'about',
+      },
+      {
+        id: 2,
+        path: '/menu',
+        name: 'menu',
+      },
+      {
+        id: 3,
+        path: '/contact',
+        name: 'contact',
+      },
+    ],
+  };
+
   render() {
-    return <div>content from navbar links </div>;
+    return (
+      <ul className="link-wrapper" open={this.props.navbarOpen}>
+        {this.state.links.map(link => {
+          return (
+            <li key={link.id}>
+              <Link to={link.path} className="nav-link">
+                {link.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    );
   }
 }
 
