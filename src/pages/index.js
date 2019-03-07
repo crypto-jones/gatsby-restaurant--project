4ef2e23d873a5287fef3c0165f8297157e775d44
crypto-jones <integral.being@gmail.com>
2019-03-06 19:34:10 -0800

    Build banner button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import { HomeHeader } from '../components/globals/header/Header';
 import Banner from '../components/globals/banner/Banner';
+import '../sass/main.scss';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <HomeHeader>
-      <Banner title="eatery" subtitle="55 Main Street — Santa Monica, CA" />
+      <Banner title="eatery" subtitle="55 Main Street — Santa Monica, CA">
+        <button className="banner-button">menu</button>
+      </Banner>
     </HomeHeader>
   </Layout>
 );
